4431bb373650532a59a0fcaef1e2202f5253cb9d
Miguel <mavargass@coreo.iue.edu.co>
2023-05-11 13:51:24 -0400

    form using grid

--- src/components/form.component.tsx
+++ src/components/form.component.tsx
@@ -1,36 +1,38 @@
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Card, CardContent, Grid } from "@mui/material"
+
 import React, { useState } from 'react';
+
 
 const FormComponent: React.FC = () => {
 
-  const [enteredName,        setEnteredName]        = useState('');
-  const [enteredLastName,    setEnteredLastName]    = useState('');
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredLastName, setEnteredLastName] = useState('');
   const [enteredPhoneNumber, setEnteredPhoneNumber] = useState('');
-  const [enteredEmail,       setEnteredEmail]       = useState('');
-  const [enteredPassword,    setEnteredPassword]    = useState('');
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
 
-  const nameInputHandler        = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredName(event.target.value);
-  const lastNameInputHandler    = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredLastName(event.target.value);
+  const nameInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredName(event.target.value);
+  const lastNameInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredLastName(event.target.value);
   const phoneNumberInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredPhoneNumber(event.target.value);
-  const emailInputHandler       = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredEmail(event.target.value);
-  const passwordInputHandler    = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredPassword(event.target.value);
+  const emailInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredEmail(event.target.value);
+  const passwordInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredPassword(event.target.value);
 
   const formSubmission = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (formValidation()) {
-    console.log('GO AND SAVE IN THE DB')
-    setEnteredName('');
-    setEnteredLastName('');
-    setEnteredPhoneNumber('');
-    setEnteredEmail('');
-    setEnteredPassword('');
+      console.log('GO AND SAVE IN THE DB')
+      setEnteredName('');
+      setEnteredLastName('');
+      setEnteredPhoneNumber('');
+      setEnteredEmail('');
+      setEnteredPassword('');
     } else {
       prompt('bad form');
     }
   }
 
-  const formValidation = () : Boolean => {
+  const formValidation = (): Boolean => {
     console.log(enteredName);
     console.log(enteredLastName);
     console.log(enteredPhoneNumber);
@@ -40,20 +42,61 @@ const FormComponent: React.FC = () => {
   }
 
   return (
-    <form onSubmit={formSubmission}>
-      <div className='form-control'>
-        <TextField id="outlined-basic" label="First name" variant="filled"   onChange={nameInputHandler} />
-        <TextField id="outlined-basic" label="Last name" variant="filled"    onChange={lastNameInputHandler} />
-        <TextField id="outlined-basic" label="Phone number" variant="filled" onChange={phoneNumberInputHandler} />
-        <TextField id="outlined-basic" label="Email" variant="filled"        onChange={emailInputHandler} />
-        <TextField id="outlined-basic" label="Password" variant="filled"     onChange={passwordInputHandler} />
-      </div>
-      <div className="form-actions">
-        <Button type="submit" variant="contained" className="button-form">
-          Save
-        </Button>
-      </div>
-    </form>
+    <Card>
+      <CardContent>
+        <form onSubmit={formSubmission}>
+        <Grid container spacing={1}>
+          <Grid xs={6} item>
+            <TextField id="outlined-basic" label="First name" size='small' variant="outlined" onChange={nameInputHandler} fullWidth />
+          </Grid>
+          <Grid xs={6} item>
+            <TextField id="outlined-basic" label="Last name" size='small' variant="outlined" onChange={lastNameInputHandler} fullWidth />
+          </Grid>
+          <Grid xs={12} item>
+            <TextField id="outlined-basic" type="text" label="Phone number" size='small' variant="outlined" onChange={phoneNumberInputHandler} fullWidth/>
+          </Grid>
+          <Grid xs={12} item>
+            <TextField id="outlined-basic" type="email" label="Email" size='small' variant="outlined" onChange={emailInputHandler} fullWidth/>
+          </Grid>
+          <Grid xs={12} item>
+            <Button variant="contained" type="submit" fullWidth>Submit</Button>
+          </Grid>
+        </Grid>
+        </form>
+
+      </CardContent>
+    </Card>
+    // <form onSubmit={formSubmission}>
+
+    //   <Grid container spacing={2}>
+    //   <Grid container spacing={2}>
+    //     <Grid item xs={5}>
+    //       <TextField id="outlined-basic" label="First name" size='small' variant="filled" onChange={nameInputHandler} />
+    //     </Grid>
+    //     <Grid item xs={5}>
+    //       <TextField id="outlined-basic" label="Last name" size='small' variant="filled" onChange={lastNameInputHandler} />
+    //     </Grid>
+    //   </Grid>
+
+    //   <Grid container spacing={2}>
+    //     <Grid item xs={10}>
+    //       <TextField id="outlined-basic" label="Phone number" size='small' variant="filled" onChange={phoneNumberInputHandler} />
+    //     </Grid>
+    //     <Grid item xs={10}>
+    //       <TextField id="outlined-basic" label="Email" size='small' variant="filled" onChange={emailInputHandler} />
+    //     </Grid>
+    //     <Grid item xs={10}>
+    //       <TextField id="outlined-basic" label="Password" size='small' variant="filled" onChange={passwordInputHandler} />
+    //     </Grid>
+    //   </Grid>
+    //   <Grid container spacing={2}>
+    //     <Button type="submit" variant="contained" className="button-form">
+    //       Save
+    //     </Button>
+    //   </Grid>
+    //   </Grid>
+
+    // </form>
   );
 };
 
