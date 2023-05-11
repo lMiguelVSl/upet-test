6157cfa1a7c84702044ffed1531d261c5243056b
Miguel <mavargass@coreo.iue.edu.co>
2023-05-11 15:46:34 -0400

    string helper and phone format

--- src/components/form.component.tsx
+++ src/components/form.component.tsx
@@ -1,6 +1,8 @@
-import { TextField, Button, Card, CardContent, Grid } from "@mui/material"
-
+import { TextField, Button, Card, CardContent, Grid, Typography, InputAdornment } from "@mui/material"
 import React, { useState } from 'react';
+import { capitalize, formatPhoneNumber } from "../helpers/string-helper";
+
+import flag from "../assets/Img/us_flag.jpg";
 
 
 const FormComponent: React.FC = () => {
@@ -11,12 +13,41 @@ const FormComponent: React.FC = () => {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
 
-  const nameInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredName(event.target.value);
-  const lastNameInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredLastName(event.target.value);
-  const phoneNumberInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredPhoneNumber(event.target.value);
-  const emailInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredEmail(event.target.value);
-  const passwordInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => setEnteredPassword(event.target.value);
+  const [valName, setValName] = useState('');
+  const [valLastName, setValLastName] = useState('');
+  const [valPhone, setValPhone] = useState('');
+  const [valEmail, setValEmail] = useState('');
+  const [valPassword, setValPassword] = useState('');
 
+  const nameInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let nameVal: string = event.target.value;
+    if (nameVal.length > 0) setEnteredName(capitalize(nameVal));
+    else setEnteredName(nameVal);
+    setValName('');
+  }
+  const lastNameInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let lastNameVal: string = event.target.value;
+    if (lastNameVal.length > 0) setEnteredLastName(capitalize(lastNameVal));
+    else setEnteredLastName(lastNameVal);
+    setValLastName('');
+  }
+  const phoneNumberInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length === 10) {
+      let numberFormatted: string = formatPhoneNumber(event.target.value);
+      setEnteredPhoneNumber(numberFormatted || '');
+    } else {
+      setEnteredPhoneNumber(event.target.value);
+    }
+    setValPhone('');
+  }
+  const emailInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredEmail(event.target.value);
+    setValEmail('');
+  }
+  const passwordInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredPassword(event.target.value);
+    setValPassword('');
+  }
   const formSubmission = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -28,12 +59,20 @@ const FormComponent: React.FC = () => {
       setEnteredEmail('');
       setEnteredPassword('');
     } else {
-      prompt('bad form');
+      console.log('bad form');
     }
   }
 
   const formValidation = (): Boolean => {
     console.log(enteredName);
+    if (!enteredName || !enteredName.length) {
+      setValName("Name is required");
+      return false;
+    }
+    if (!enteredLastName || !enteredLastName.length) {
+      setValLastName("Last Name is required");
+      return false;
+    }
     console.log(enteredLastName);
     console.log(enteredPhoneNumber);
     console.log(enteredEmail);
@@ -42,61 +81,93 @@ const FormComponent: React.FC = () => {
   }
 
   return (
-    <Card>
+    <Card style={{ maxWidth: 500, margin: "0 auto", padding: "20px 5px" }}>
       <CardContent>
+        <Typography gutterBottom variant="h4">uPet</Typography>
         <form onSubmit={formSubmission}>
-        <Grid container spacing={1}>
-          <Grid xs={6} item>
-            <TextField id="outlined-basic" label="First name" size='small' variant="outlined" onChange={nameInputHandler} fullWidth />
+          <Grid container spacing={1}>
+            <Grid xs={6} item>
+              <TextField
+                id="outlined-basic"
+                type="text"
+                label="First name"
+                size='small'
+                variant="outlined"
+                onChange={nameInputHandler}
+                value={enteredName}
+                error={valName && valName.length ? true : false}
+                helperText={valName}
+                fullWidth />
+            </Grid>
+            <Grid xs={6} item>
+              <TextField
+                id="outlined-basic"
+                type="text"
+                label="Last name"
+                size='small'
+                variant="outlined"
+                onChange={lastNameInputHandler}
+                value={enteredLastName}
+                error={valLastName && valLastName.length ? true : false}
+                helperText={valLastName}
+                fullWidth />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                id="outlined-basic"
+                type="tel"
+                label="Phone number"
+                size='small'
+                InputProps={{
+                  inputProps: { maxLength: 10, inputMode: 'tel' },
+                  startAdornment:
+                    <InputAdornment position="start">
+                      <img src={flag} />
+                    </InputAdornment>
+
+                }}
+                variant="outlined"
+                onChange={phoneNumberInputHandler}
+                value={enteredPhoneNumber}
+                error={valPhone && valPhone.length ? true : false}
+                helperText={valPhone}
+                fullWidth />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                id="outlined-basic"
+                type="email"
+                label="Email"
+                size='small'
+                variant="outlined"
+                onChange={emailInputHandler}
+                value={enteredEmail}
+                error={valEmail && valEmail.length ? true : false}
+                helperText={valEmail}
+                fullWidth />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                id="outlined-basic"
+                type="password"
+                label="Password"
+                size='small'
+                variant="outlined"
+                onChange={passwordInputHandler}
+                value={enteredPassword}
+                error={valPassword && valPassword.length ? true : false}
+                helperText={valPassword}
+                fullWidth />
+            </Grid>
+            <Grid xs={12} item>
+              <Button variant="contained"
+                type="submit" fullWidth>Submit</Button>
+            </Grid>
           </Grid>
-          <Grid xs={6} item>
-            <TextField id="outlined-basic" label="Last name" size='small' variant="outlined" onChange={lastNameInputHandler} fullWidth />
-          </Grid>
-          <Grid xs={12} item>
-            <TextField id="outlined-basic" type="text" label="Phone number" size='small' variant="outlined" onChange={phoneNumberInputHandler} fullWidth/>
-          </Grid>
-          <Grid xs={12} item>
-            <TextField id="outlined-basic" type="email" label="Email" size='small' variant="outlined" onChange={emailInputHandler} fullWidth/>
-          </Grid>
-          <Grid xs={12} item>
-            <Button variant="contained" type="submit" fullWidth>Submit</Button>
-          </Grid>
-        </Grid>
         </form>
 
       </CardContent>
     </Card>
-    // <form onSubmit={formSubmission}>
-
-    //   <Grid container spacing={2}>
-    //   <Grid container spacing={2}>
-    //     <Grid item xs={5}>
-    //       <TextField id="outlined-basic" label="First name" size='small' variant="filled" onChange={nameInputHandler} />
-    //     </Grid>
-    //     <Grid item xs={5}>
-    //       <TextField id="outlined-basic" label="Last name" size='small' variant="filled" onChange={lastNameInputHandler} />
-    //     </Grid>
-    //   </Grid>
-
-    //   <Grid container spacing={2}>
-    //     <Grid item xs={10}>
-    //       <TextField id="outlined-basic" label="Phone number" size='small' variant="filled" onChange={phoneNumberInputHandler} />
-    //     </Grid>
-    //     <Grid item xs={10}>
-    //       <TextField id="outlined-basic" label="Email" size='small' variant="filled" onChange={emailInputHandler} />
-    //     </Grid>
-    //     <Grid item xs={10}>
-    //       <TextField id="outlined-basic" label="Password" size='small' variant="filled" onChange={passwordInputHandler} />
-    //     </Grid>
-    //   </Grid>
-    //   <Grid container spacing={2}>
-    //     <Button type="submit" variant="contained" className="button-form">
-    //       Save
-    //     </Button>
-    //   </Grid>
-    //   </Grid>
-
-    // </form>
   );
 };
 
---
+++ src/helpers/string-helper.ts
@@ -0,0 +1,10 @@
+export const capitalize = (_string: string) => {
+    return _string[0].toUpperCase() + _string.slice(1);
+}
+
+export const formatPhoneNumber = (phoneNumber: string) => {
+    const cleaned = ('' + phoneNumber).replace(/\D/g, '');
+    const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    return match ? '(' + match[1] + ') ' + match[2] + '-' + match[3] : '';
+
+}
