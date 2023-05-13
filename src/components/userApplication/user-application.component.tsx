0c297ee96b5ade667f303500b854e8e95daf2efa
Miguel <mavargass@coreo.iue.edu.co>
2023-05-13 12:21:58 -0400

    getting the id to search the user info

--- src/components/userApplication/user-application.component.tsx
+++ src/components/userApplication/user-application.component.tsx
@@ -1,7 +1,13 @@
+import { useParams } from 'react-router-dom';
+
 const UserApplication = () => {
+    const { id } = useParams();
     return (
-        <div>USER APPLICATION WORKS.</div>
-);
+        <>
+            <div>USER APPLICATION WORKS.</div>
+            <h1>{id}</h1>
+        </>
+    );
 }
 
 export default UserApplication;
